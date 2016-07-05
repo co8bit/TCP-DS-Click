22fffbfccda2679b9d58c44da7db71662eace122
co8bit <printffile@gmail.com>
2016-07-05 18:04:03 +0800

    test

--- src/gendata.js
+++ src/gendata.js
@@ -10,17 +10,9 @@ require('shelljs/global');
  * @date    2016-07-05
  */
 run = () => {
-	console.log('==1==');
-	shell.exec('ls');
-	console.log('==2==');
-	console.log('cd ' + CONFIG.config.dsdgen_dir);
-	shell.exec('cd ' + CONFIG.config.dsdgen_dir);
 	cd(CONFIG.config.dsdgen_dir);
-	console.log('==3==');
-	shell.exec('ls');
-	console.log('==4==');
 	shell.exec('./dsdgen –scale ' + CONFIG.config.scale + ' -dir ' + CONFIG.config.dsdgen_output_dir);
-	console.log('==5==');
+	console.log('./dsdgen –scale ' + CONFIG.config.scale + ' -dir ' + CONFIG.config.dsdgen_output_dir);
 	//./dsqgen -input ../query_templates/templates.lst -directory ../query_templates -scale 1 -output_dir ../wbx
 	// shell.exec('./dsqgen -input ' + CONFIG.config.scale + ' -dir ' + CONFIG.config.dsdgen_output_dir);
 	// console.log('cd ../' + CONFIG.config.dsdgen_dir);
