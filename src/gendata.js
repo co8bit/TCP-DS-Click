ba4880ff34fa75c87a8145a5d6e4f0fdc1f4f5ca
co8bit <printffile@gmail.com>
2016-07-06 10:41:05 +0800

    fix bug

--- src/gendata.js
+++ src/gendata.js
@@ -19,7 +19,7 @@ run = () => {
 	{
 		console.log('./dsdgen -force Y –scale ' + CONFIG.config.scale
 				+ ' -dir ' + rootPath + CONFIG.config.dsdgen_output_dir
-				+ '-parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' & ';
+				+ '-parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' & ');
 		shell.exec('./dsdgen -force Y –scale ' + CONFIG.config.scale
 				+ ' -dir ' + rootPath + CONFIG.config.dsdgen_output_dir
 				+ '-parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' & '
