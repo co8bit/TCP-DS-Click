6bfd8fdb3627b6126b0519959c2197e46c7b6fc3
co8bit <printffile@gmail.com>
2016-07-08 15:30:51 +0800

    添加sql生成

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var CONFIG = require('./config/config');
 var Timer = require('./src/timer');
 var gendata = require('./src/gendata');
+var gensql = require('./src/gensql');
 
 var timer = Timer.Timer.create();
 
 
 console.log("<===========================>  TCP-DS Click Run  <===========================>");
+var gensqlPromise = new Promise((resolve,reject) => {});
 gendata.run().then(() => {
-	// gensql.run;
+	gensqlPromise = gensql.run();
+});
+
+gensqlPromise.then(() => {
+	console.log('gensql==--+++123');
 });
