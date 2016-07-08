af66db00bcbf969b1fecee6b725ec1127b285b99
co8bit <printffile@gmail.com>
2016-07-08 11:10:56 +0800

    添加promis

--- src/gendata.js
+++ src/gendata.js
@@ -10,42 +10,54 @@ require('shelljs/global');
  * @date    2016-07-05
  */
 run = () => {
+	var dsdgenPromise = [];
+
 	var rootPath = process.cwd();
 	console.log('rootPath:'+rootPath);
-
 	cd(CONFIG.config.dsdgen_dir);
 
 	for (var i = 0; i < CONFIG.config.parallel; i++)
 	{
-		var cmdStr = './dsdgen –scale ' + CONFIG.config.scale
+		dsdgenPromise[i] = new Promise( (resolve,reject) => {
+			var cmdStr = '命令：./dsdgen –scale ' + CONFIG.config.scale
 				+ ' -dir ' + rootPath + CONFIG.config.dsdgen_output_dir
-				+ ' -parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' &';
-		console.log('cmdStr:'+cmdStr);
-		// shell.exec('./dsdgen –scale ' + CONFIG.config.scale
-		// 		+ ' -dir ' + rootPath + CONFIG.config.dsdgen_output_dir
-		// 		+ ' -parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' &'
-		// 	);
+				+ ' -parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' & ';
+			console.log('cmdStr:'+cmdStr);
+			// shell.exec('./dsdgen –scale ' + CONFIG.config.scale
+			// 		+ ' -dir ' + rootPath + CONFIG.config.dsdgen_output_dir
+			// 		+ ' -parallel ' + CONFIG.config.parallel + ' -child ' + (i+1) + ' &'
+			// 	);
 
-		var exec = require('child_process').exec; 
-		exec(cmdStr, function(err,stdout,stderr,i){
-		  if(err) {
-		    console.log('get weather api error:'+stderr);
-		  } else {
-		  	console.log('============');
-		  	console.log('i: '+i+' start');
-		  	console.log('stdout:');
-		  	console.log(stdout);
-		  	console.log('stderr:');
-		  	console.log(stderr);
-		  	console.log('over');
-		  	console.log('============');
-		  }
-		});
+			var execAsyn = require('child_process').exec; 
+			execAsyn(cmdStr, function(err,stdout,stderr){
+				if(err)
+				{
+					reject(stderr);
+				}
+				else
+				{
+				  	resolve(stderr);
+				  	// console.log('============');
+				  	// console.log('stdout:');
+				  	// console.log(stdout);
+				  	// console.log('stderr:');
+				  	// console.log(stderr);
+				  	// console.log('over');
+				  	// console.log('============');
+				}
+			});//exeAsyn
+		})//promise
+	}//for
 
-	}
-	// dsdgen –scale 100 –dir /tmp –parallel 4 –child 1 &	
-	// dsdgen –scale 100 –dir /tmp –parallel 4 –child 2 &
+	Promise.all(dsdgenPromise).then((stdout) => {
+		console.log(stdout);
+	}).catch((error) => {
+		console.log('error:'+error);
+	});
 
+
+
+	
 
 	//./dsqgen -input ../query_templates/templates.lst -directory ../query_templates -scale 1 -output_dir ../wbx
 	// shell.exec('./dsqgen -input ' + CONFIG.config.scale + ' -dir ' + CONFIG.config.dsdgen_output_dir);
