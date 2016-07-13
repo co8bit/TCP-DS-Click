16a60dfff0834d8bc87b49f124eb20dc91b9de5a
co8bit <printffile@gmail.com>
2016-07-13 17:44:09 +0800

    test

--- src/load_monetdb.js
+++ src/load_monetdb.js
@@ -11,18 +11,16 @@ run = (rootPath) => {
 			console.log('===============');
 			console.log('===============');
 
-			// cd(rootPath + CONFIG.config.dsdgen_output_dir);
 			path = rootPath + CONFIG.config.dsdgen_output_dir;
-
 			var imp = new Importer(dbOptions,{locked:false},path+'call_center_1_4.dat' , 'call_center');
-		 
 			imp.import(function(err) {
 				if(err) {
 					reject(new Error(
 							'Could not import file '+ path+'call_center_1_4.dat' +' Reason: '+err
 						));
 				}
-
+				console.console.log('importedRows :'+importedRows );
+				console.console.log('rejectedRows  :'+rejectedRows  );
 				console.log(path+'call_center_1_4.dat' + 'successfully imported into database table call_center');
 				resolve();
 			});
