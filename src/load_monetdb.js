0c84de0161207c9b967a85ebf392ed013f7427a3
co8bit <printffile@gmail.com>
2016-07-13 17:12:41 +0800

    修改monetdb-import，使之不创建新表

--- src/load_monetdb.js
+++ src/load_monetdb.js
@@ -6,30 +6,32 @@ var dbOptions = {
 }
 
 run = (rootPath) => {
-	try {
-		console.log('===============');
-		console.log(dbOptions);
-		console.log('===============');
+	return new Promise ( (resolve,reject) => {
+		try{
+			console.log('===============');
+			console.log('===============');
 
-		// cd(rootPath + CONFIG.config.dsdgen_output_dir);
-		path = rootPath + CONFIG.config.dsdgen_output_dir;
+			// cd(rootPath + CONFIG.config.dsdgen_output_dir);
+			path = rootPath + CONFIG.config.dsdgen_output_dir;
 
-		var imp = new Importer(dbOptions, path+'call_center_1_4.dat' , 'call_center');
-	 
-		imp.import(function(err) {
-			if(err) {
-				console.log('Could not import file '+ path+'call_center_1_4.dat' +' Reason: '+err);
-				reject();
-			}
-	 
-			console.log(path+'call_center_1_4.dat' + 'successfully imported into database table call_center');
-		});
-	} catch(e) {
-		// Could not construct the importer object. Possible reasons:  
-		// 1) Invalid parameters 
-		// 2) file not found  
-		// 3) file is binary 
-		console.log(e.message);
+			var imp = new Importer(dbOptions, path+'call_center_1_4.dat' , 'call_center');
+		 
+			imp.import(function(err) {
+				if(err) {
+					console.log('Could not import file '+ path+'call_center_1_4.dat' +' Reason: '+err);
+					reject();
+				}
+		 
+				console.log(path+'call_center_1_4.dat' + 'successfully imported into database table call_center');
+				resolve();
+			});
+		}catch(e){
+			// Could not construct the importer object. Possible reasons:  
+			// 1) Invalid parameters 
+			// 2) file not found  
+			// 3) file is binary 
+			console.log(e.message);
+		}
 	}
 }
 
