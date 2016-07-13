e8f798dc941d47ade0e22024b0ad69cb9aeb657d
co8bit <printffile@gmail.com>
2016-07-13 10:53:30 +0800

    fix

--- src/load_monetdb.js
+++ src/load_monetdb.js
@@ -12,14 +12,14 @@ run = (rootPath) => {
 		cd(rootPath + CONFIG.config.dsdgen_output_dir);
 		console.log('path:'+rootPath + CONFIG.config.dsdgen_output_dir);
 
-		var imp = new Importer(dbOptions, './' , 'call_center_1_4.dat');
+		var imp = new Importer(dbOptions, 'call_center_1_4.dat' , 'call_center');
 	 
 		imp.import(function(err) {
 			if(err) {
 				console.log('Could not import file '+ 'call_center_1_4.dat' +' Reason: '+err);
 			}
 	 
-			console.log('call_center_1_4.dat' + 'successfully imported into database table call_center_1_4.dat');
+			console.log('call_center_1_4.dat' + 'successfully imported into database table call_center');
 		});
 	} catch(e) {
 		// Could not construct the importer object. Possible reasons:  
