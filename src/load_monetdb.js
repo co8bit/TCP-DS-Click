3934536f7168405abb504ce96481f91eec1f194d
co8bit <printffile@gmail.com>
2016-07-14 10:39:34 +0800

    test

--- src/load_monetdb.js
+++ src/load_monetdb.js
@@ -10,17 +10,7 @@ run = (rootPath) => {
 	return new Promise ( (resolve,reject) => {
 		var importPromise = [];
 		path = rootPath + CONFIG.config.dsdgen_output_dir;
-		for(var tableName of CONFIG.config._TABLE_NAME)
-		{
-			for (var i = 1; i <= CONFIG.config.parallel; i++)
-			{
-					// console.log('===============');
-					var file = path + tableName + '_' + i + '_' + CONFIG.config.parallel + '.dat';
-					if (!fs.existsSync(file))
-						continue;
-					
-			}
-		}
+		
 		
 		Promise.all(importPromise).then( () => {
 			resolve('all ok');
