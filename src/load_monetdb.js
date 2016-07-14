debe008ec1f90b5fb8945631158994a301abbd25
co8bit <printffile@gmail.com>
2016-07-14 10:19:08 +0800

    多表导入

--- src/load_monetdb.js
+++ src/load_monetdb.js
@@ -1,5 +1,6 @@
 var CONFIG = require('../config/config');
 var Importer = require('monetdb-import')();
+var fs = require('fs');
  
 var dbOptions = {
 	dbname: CONFIG.db.monetdb.dbName,
@@ -7,32 +8,37 @@ var dbOptions = {
 
 run = (rootPath) => {
 	return new Promise ( (resolve,reject) => {
-		try{
-
-			path = rootPath + CONFIG.config.dsdgen_output_dir;
-			var imp = new Importer(dbOptions,{locked:false},path+'call_center_1_4.dat' , 'call_center');
-			imp.import(function(err,info) {
-				if(err)
-				{
-					reject(new Error(
-							'Could not import file '+ path+'call_center_1_4.dat' +' Reason: '+err
-						));
+		path = rootPath + CONFIG.config.dsdgen_output_dir;
+		for(var tableName of CONFIG.config._TABLE_NAME)
+		{
+			for (var i = 1; i <= CONFIG.config.parallel; i++)
+			{
+				try{
+					// console.log('===============');
+					var file = path + tableName + '_' + i + '_' + CONFIG.config.parallel + '.dat';
+					if (!fs.existsSync(file))
+						continue;
+					var imp = new Importer(dbOptions,{locked:false},file,tableName);
+					imp.import(function(err,info) {
+						if(err)
+						{
+							reject(new Error('Could not import file '+ file +' Reason: '+err));
+						}
+						else
+						{
+							console.log(file + '  成功导入'+info.importedRows+'条，被拒绝'+info.rejectedRows+'条。'+"\n");
+						}
+					});
+				}catch(e){
+					// Could not construct the importer object. Possible reasons:  
+					// 1) Invalid parameters 
+					// 2) file not found  
+					// 3) file is binary 
+					console.log(e.message);
 				}
-				else
-				{
-					console.log('===============');
-					console.log('===============');
-					console.log(path+'call_center_1_4.dat' + '  成功导入'+info.importedRows+'条，被拒绝'+info.rejectedRows+'条。'+"\n");
-					resolve();
-				}
-			});
-		}catch(e){
-			// Could not construct the importer object. Possible reasons:  
-			// 1) Invalid parameters 
-			// 2) file not found  
-			// 3) file is binary 
-			console.log(e.message);
+			}
 		}
+		resolve();
 	})
 }
 
