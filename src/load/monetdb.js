e7012f58fcb763e6ee9b8baf35dcb04238f5bd72
co8bit <printffile@gmail.com>
2016-07-19 17:24:04 +0800

    t

--- src/load/monetdb.js
+++ src/load/monetdb.js
@@ -18,19 +18,20 @@ load = (file,tableName) => {
 		util.log('jinru1','jinru1');
 		conn.query("COPY INTO "+ tableName +" FROM "+ file +" USING DELIMITERS '|','\n' NULL AS '';")
 		.then(function(result) {
-			conn.query("SELECT COUNT(DISTINCT rowid) FROM sys.rejects").then((res1) => {
-				conn.query("SELECT COUNT(*) FROM " + _getTablename()).then( (res2) => {
-					rejectedRows = res1.state === "fulfilled" ? res1 : -1;
-					importedRows = res2.state === "fulfilled" ? res2 : -1;
+			// conn.query("SELECT COUNT(DISTINCT rowid) FROM sys.rejects").then((res1) => {
+			// 	conn.query("SELECT COUNT(*) FROM " + _getTablename()).then( (res2) => {
+			// 		rejectedRows = res1.state === "fulfilled" ? res1 : -1;
+			// 		importedRows = res2.state === "fulfilled" ? res2 : -1;
 					
 
-					util.log(res1,'res1');
-					util.log(res2,'res2');
-					util.log(rejectedRows,'rejectedRows');
-					util.log(importedRows,'importedRows');
-					resolve(timer.end());
-				})
-			})
+			// 		util.log(res1,'res1');
+			// 		util.log(res2,'res2');
+			// 		util.log(rejectedRows,'rejectedRows');
+			// 		util.log(importedRows,'importedRows');
+					util.log(tableName+'ok',tableName+'ok');
+					resolve();
+				// })
+			// })
                     
                     // result.rejectedRows = d[0].state === "fulfilled" ? d[0].value.data[0][0] : -1;
                     // result.importedRows = d[1].state === "fulfilled" ? d[1].value.data[0][0] : -1;
