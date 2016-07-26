1a81aa699754201abb70c0101dc7a55045768437
co8bit <printffile@gmail.com>
2016-07-26 10:35:23 +0800

    test

--- index.js
+++ index.js
@@ -85,19 +85,19 @@ Promise.resolve()
 /**
  * throughput test module ---------------
  */
-.then( (useTime) => {
-	//monetdb
-	console.log('throughputTest_monetdb................................RUN');
-	var tmpPromise = throughputTest_monetdb.run(rootPath,statistics);
-	tmpPromise.then( (useTime) => {
-		console.log('throughputTest_monetdb................................OK');
-		console.log('throughputTest_monetdb time :' + useTime + 's');
-		statistics.throughputTest_monetdb = useTime;
-		console.log('throughputTest_monetdbArray:');
-		console.log(statistics.throughputTest_monetdbArray);
-	});
-	return tmpPromise;
-})
+// .then( (useTime) => {
+// 	//monetdb
+// 	console.log('throughputTest_monetdb................................RUN');
+// 	var tmpPromise = throughputTest_monetdb.run(rootPath,statistics);
+// 	tmpPromise.then( (useTime) => {
+// 		console.log('throughputTest_monetdb................................OK');
+// 		console.log('throughputTest_monetdb time :' + useTime + 's');
+// 		statistics.throughputTest_monetdb = useTime;
+// 		console.log('throughputTest_monetdbArray:');
+// 		console.log(statistics.throughputTest_monetdbArray);
+// 	});
+// 	return tmpPromise;
+// })
 
 
 
