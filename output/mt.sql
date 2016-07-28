d11d64ff90d16b4917dd479945325412d445e86c
co8bit <printffile@gmail.com>
2016-07-28 14:56:11 +0800

    fix

--- output/mt.sql
+++ output/mt.sql
@@ -35,33 +35,31 @@
 --
 
 use youdata;
-drop table dbgen_version;
-drop table customer_address;
-drop table customer_demographics;
-drop table date_dim;
-drop table warehouse;
-drop table ship_mode;
-drop table time_dim;
-drop table reason;
-drop table income_band;
-drop table item;
-drop table store;
-drop table call_center;
-drop table customer;
-drop table web_site;
-drop table store_returns;
-drop table household_demographics;
-drop table web_page;
-drop table promotion;
-drop table catalog_page;
-drop table inventory;
-drop table catalog_returns;
-drop table web_returns;
-drop table web_sales;
-drop table catalog_sales;
-drop table store_sales;
-
-
+DROP TABLE IF EXISTS `dbgen_version`;
+DROP TABLE IF EXISTS `customer_address`;
+DROP TABLE IF EXISTS `customer_demographics`;
+DROP TABLE IF EXISTS `date_dim`;
+DROP TABLE IF EXISTS `warehouse`;
+DROP TABLE IF EXISTS `ship_mode`;
+DROP TABLE IF EXISTS `time_dim`;
+DROP TABLE IF EXISTS `reason`;
+DROP TABLE IF EXISTS `income_band`;
+DROP TABLE IF EXISTS `item`;
+DROP TABLE IF EXISTS `store`;
+DROP TABLE IF EXISTS `call_center`;
+DROP TABLE IF EXISTS `customer`;
+DROP TABLE IF EXISTS `web_site`;
+DROP TABLE IF EXISTS `store_returns`;
+DROP TABLE IF EXISTS `household_demographics`;
+DROP TABLE IF EXISTS `web_page`;
+DROP TABLE IF EXISTS `promotion`;
+DROP TABLE IF EXISTS `catalog_page`;
+DROP TABLE IF EXISTS `inventory`;
+DROP TABLE IF EXISTS `catalog_returns`;
+DROP TABLE IF EXISTS `web_returns`;
+DROP TABLE IF EXISTS `web_sales`;
+DROP TABLE IF EXISTS `catalog_sales`;
+DROP TABLE IF EXISTS `store_sales`;
 
 
 
